0896b893fcefb897040ffa29639e961d0ceda0e4
Rodriguez Chaparro <diego.rodriguez_chaparro@siemens.com>
2019-04-05 16:46:37 -0500

    Login needs to test post

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+
+  login() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    alert(email.innerHTML);
+  }
 
   ngOnInit() {
   }
